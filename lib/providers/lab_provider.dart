2eacb1dbe5e8ac3fa45e10c93e6e8277c024c0da
rudra273 <rudramohanty45@gmail.com>
2026-09-25 06:39:15 +0530

    p5

--- lib/providers/lab_provider.dart
+++
@@ -1,3 +0,0 @@
-/// The virtual lab is available to every board from Class 7 through Class 12.
-bool labAvailableForSelection(Iterable<int> selectedClasses) =>
-    selectedClasses.any((classNo) => classNo >= 7 && classNo <= 12);
